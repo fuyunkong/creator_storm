332dfb5f69e1790ec8043b471d1305353cabf72d
wuliang(jhxc) <1780095771@qq.com>
2017-04-27 09:09:43 +0800

    备份

--- packages/stateeditor/panel/js/control/TreeControl.js
+++ packages/stateeditor/panel/js/control/TreeControl.js
@@ -7,11 +7,24 @@ app.controller('tree_control', ['$scope',
 	function FishAngular($scope) {
 
 		//侧边栏
-		$scope.slide_on = function (item) {
+		$scope.slide_add = function (item) {
 			tolog(item);
 			tolog(silde_data);
-			silde_data.children.push(slide_getitem());
-			$scope.$apply();
+			item.children.push(slide_getitem());
+
+		}
+		$scope.slide_delete = function (item) {
+			// item.children.push(slide_getitem());
+			console.log(item.$$hashKey);
+			var obj_msg = item.$$hashKey;
+			if(obj_msg === undefined){
+				silde_data.children= [];
+			}else{
+
+			}
+			tolog(silde_data);
+
+
 		}
 
 	}]);
